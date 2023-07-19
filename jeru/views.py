571f5c036dc2f51cb33bccfdfcaf980b9978b6a5
jerusha <yeasupoguaswitha@gmail.com>
2023-07-19 10:22:36 +0530

    fetched data from db

---
+++ jeru/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import post
+
+# Create your views here.
+
+def index(request):
+
+    posts = post.objects.all()
+
+    print(posts)
+
+    return render(request,'index.html',context={'posts':posts})
+
+def about(request):
+    return render(request,'about.html')
